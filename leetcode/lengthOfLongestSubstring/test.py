dc04ec4b8c212a80b9612b5d9e9a1ad004ccab30
w4bo <m.francia@unibo.it>
2021-11-23 21:31:48 +0100

    fix equals

--- leetcode/lengthOfLongestSubstring/test.py
+++ leetcode/lengthOfLongestSubstring/test.py
@@ -5,12 +5,12 @@ sys.path.insert(0, 'lengthOfLongestSubstring')
 from v1 import test_function
 
 def unit_test(self, test_function):
-    self.assertEquals(test_function("abc"), 3)
-    self.assertEquals(test_function("ababc"), 3)
-    self.assertEquals(test_function("abcabcbb"), 3)
-    self.assertEquals(test_function("bbbbb"), 1)
-    self.assertEquals(test_function("pwwkew"), 3)
-    self.assertEquals(test_function("dvdf"), 3)
+    self.assertEqual(test_function("abc"), 3)
+    self.assertEqual(test_function("ababc"), 3)
+    self.assertEqual(test_function("abcabcbb"), 3)
+    self.assertEqual(test_function("bbbbb"), 1)
+    self.assertEqual(test_function("pwwkew"), 3)
+    self.assertEqual(test_function("dvdf"), 3)
 
 class MyTest(unittest.TestCase):
     def test(self):
--- leetcode/longestPalindrome/test.py
+++ leetcode/longestPalindrome/test.py
@@ -6,18 +6,18 @@ from v1 import test_function
 
 
 def unit_test(self, test_function):
-    self.assertEquals(test_function("daaaaaaa"), "aaaaaaa")
-    self.assertEquals(test_function("dadbbbbada"), "bbbb")
-    self.assertEquals(test_function("babad"), "bab")
-    self.assertEquals(test_function("anna"), "anna")
-    self.assertEquals(test_function("cbbd"), "bb")
-    self.assertEquals(test_function("a"), "a")
-    self.assertEquals(test_function("bcdcb"), "bcdcb")
-    self.assertEquals(test_function("adaa"), "ada")
-    self.assertEquals(test_function("aaada"), "aaa")
-    self.assertEquals(test_function("asdasdasdbb"), "bb")
-    self.assertEquals(test_function("bb"), "bb")
-    self.assertEquals(test_function("bbb"), "bbb")
+    self.assertEqual(test_function("daaaaaaa"), "aaaaaaa")
+    self.assertEqual(test_function("dadbbbbada"), "bbbb")
+    self.assertEqual(test_function("babad"), "bab")
+    self.assertEqual(test_function("anna"), "anna")
+    self.assertEqual(test_function("cbbd"), "bb")
+    self.assertEqual(test_function("a"), "a")
+    self.assertEqual(test_function("bcdcb"), "bcdcb")
+    self.assertEqual(test_function("adaa"), "ada")
+    self.assertEqual(test_function("aaada"), "aaa")
+    self.assertEqual(test_function("asdasdasdbb"), "bb")
+    self.assertEqual(test_function("bb"), "bb")
+    self.assertEqual(test_function("bbb"), "bbb")
 
 
 class MyTest(unittest.TestCase):
