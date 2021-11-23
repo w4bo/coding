3ea0d5d7abf6ad624c90ba9ac9c02f9ddf186e33
mfrancia <matteo.francia2@studio.unibo.it>
2021-11-23 21:49:06 +0100

    working on CI

--- leetcode/test_all.py
+++ leetcode/test_all.py
@@ -9,11 +9,8 @@ class Test(unittest.TestCase):
         res = []
         for folder in [f.path for f in os.scandir(".") if f.is_dir() and "venv" not in f.path]: # iterate over all the sub-folders (i.e., coding problems)
             folder = folder.replace("\\", "").replace("/", "").replace(".", "")
-            print(folder)
             module = import_module(folder + ".test") # import the unit_test function
-            print(module)
             unit_test = module.unit_test
-            print(unit_test)
             for filename in os.listdir(folder): # iterate over all solutions of each coding problem
                 if filename.endswith(".py") and filename != "test.py": 
                     module = import_module(folder + "." + filename.replace(".py", "")) # import the function to be tested
