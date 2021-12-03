f2af96425cd2bf9a6b944a6feb61526f87e5c3df
mfrancia <matteo.francia2@studio.unibo.it>
2021-12-03 23:01:19 +0100

    update

--- adventofcode/2021/01/part1.py
+++ adventofcode/2021/01/part1.py
@@ -1,9 +1,19 @@
-with open('input.txt') as f:
-    lines = [int(x.replace("\n", "")) for x in f.readlines()]
-    c = 0
-    prev = lines[0]
-    for cur in lines[1:]:
-        if cur > prev:
-            c += 1
-        prev = cur
-print(c)
+import unittest
+
+def f():
+    with open('input.txt') as f:
+        lines = [int(x.replace("\n", "")) for x in f.readlines()]
+        c = 0
+        prev = lines[0]
+        for cur in lines[1:]:
+            if cur > prev:
+                c += 1
+            prev = cur
+    return c
+
+class Test(unittest.TestCase):
+    def test(self):
+        self.assertEqual(f(), 1752)
+
+if __name__ == "__main__":
+    unittest.main()
--- adventofcode/2021/01/part2.py
+++ adventofcode/2021/01/part2.py
@@ -1,19 +1,21 @@
-import sys
+import unittest
 
-def sum(l):
-    cur = 0
-    for x in l:
-        cur += x
-    return cur
+def f():
+    with open('input.txt') as f:
+        lines = [int(x.replace("\n", "")) for x in f.readlines()]
+        c = 0
+        prev = sum(lines[0: 3])
+        for curi in range(3, len(lines)):
+            l = lines[curi - 2: curi + 1]
+            cur = sum(l)
+            if cur > prev:
+                c += 1
+            prev = cur
+    return c
 
-with open('input.txt') as f:
-    lines = [int(x.replace("\n", "")) for x in f.readlines()]
-    c = 0
-    prev = sum(lines[0: 3])
-    for curi in range(3, len(lines)):
-        l = lines[curi - 2: curi + 1]
-        cur = sum(l)
-        if cur > prev:
-            c += 1
-        prev = cur
-print(c)
+class Test(unittest.TestCase):
+    def test(self):
+        self.assertEqual(f(), 1781)
+
+if __name__ == "__main__":
+    unittest.main()
